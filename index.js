63338816e5b94ee7b27ebec04e7e17e1c4513caa
Jatin7404 <jatinkhanijo567@gmail.com>
2025-06-04 13:19:51 +0530

    orgin changes

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://afs-frontend-jatins-projects-34f7a407.vercel.app/", // or your frontend URL
+  credentials: true
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
